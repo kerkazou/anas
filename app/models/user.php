67fd513a38eb624786ae33398fb997e6578ca928
kerkazou <kerkazou.zakaria@gmail.com>
2022-06-25 23:29:39 +0100

    .....

---
+++ app/models/user.php
@@ -0,0 +1,50 @@
+<?php
+
+class User {
+
+    private $db;
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function signup($data){
+        $this->db->query('INSERT INTO `user` (first_name, last_name, email, password) VALUES (:first_name, :last_name, :email, :password)');
+        $this->db->bind(':first_name', $data['first_name']);
+        $this->db->bind(':last_name', $data['last_name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', md5($data['password']));
+
+        if($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function userExists($data){
+        $this->db->query('SELECT * FROM `user` WHERE email = :email');
+        $this->db->bind(':email', $data['email']);
+
+        $row = $this->db->single();
+        if($row){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    
+    public function signin($data){
+        $this->db->query('SELECT * FROM `user` WHERE email = :email AND password = :password');
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', md5($data['password']));
+
+        $row = $this->db->single();
+        if($row){
+            return $row;
+        }else{
+            return false;
+        }
+    }
+}
